528e5a0c9bce9d30a3936617ed483f594192cdf7
vedantssonar-coder <vedantssonar@gmail.com>
2025-12-11 11:25:03 +0530

    Calibration testing

--- DroneCode/DroneCode.ino
+++ DroneCode/DroneCode.ino
@@ -4,43 +4,38 @@ ServoTimer2 esc[4];
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("=== ESC THROTTLE RANGE CALIBRATION ===");
-  Serial.println("DISCONNECT BATTERY NOW!");
-  Serial.println("Waiting 3 seconds...");
-  delay(3000);
+  Serial.println("Testing ESC calibration...");
+  Serial.println("Battery must be connected, props OFF");
+  delay(2000);
   
   esc[0].attach(3);
   esc[1].attach(5);
   esc[2].attach(6);
   esc[3].attach(9);
   
-  // Step 1: Send MAX throttle to all ESCs
-  Serial.println("\nStep 1: Sending MAX throttle (2000 µs)");
-  for (int i = 0; i < 4; i++) {
-    esc[i].write(2000);
-  }
-  
-  Serial.println("CONNECT BATTERY NOW - Listen for beeping");
-  Serial.println("You will hear: beep-beep-beep, beep-beep");
-  Serial.println("Wait 5 seconds for beeping to complete...");
-  delay(5000);
-  
-  // Step 2: Send MIN throttle to all ESCs
-  Serial.println("\nStep 2: Sending MIN throttle (1000 µs)");
+  // Arm at 1000
   for (int i = 0; i < 4; i++) {
     esc[i].write(1000);
   }
-  
-  Serial.println("Listen for final confirmation beeps (should be 3-4 quick beeps)");
-  Serial.println("Calibration complete!");
-  Serial.println("DISCONNECT BATTERY");
-  delay(5000);
+  delay(2000);
 }
 
 void loop() {
-  // Hold MIN throttle
+  // Slowly increase throttle from 1000 to 2000
+  for (int throttle = 1000; throttle <= 2000; throttle += 10) {
+    for (int i = 0; i < 4; i++) {
+      esc[i].write(throttle);
+    }
+    
+    Serial.print("Throttle: ");
+    Serial.print(throttle);
+    Serial.println(" µs - All motors should start together");
+    delay(500);
+  }
+  
+  // Return to min
   for (int i = 0; i < 4; i++) {
     esc[i].write(1000);
   }
-  delay(100);
+  delay(2000);
 }
